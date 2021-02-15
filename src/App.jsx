7f162a38a02e7433641bc7ba933c22ce686c7f5e
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-02-15 18:47:46 +0900

    Buttonエレメントを追加してみた

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* App.jsx */
+
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+class App extends React.Component {
+	render() {
+		return <Button>Click me!</Button>
+	}
+
+}
+export default App;
